a1733627276901de5bea1a71cebf052ea249b102
Killer_Bi_ <iosif.polodashvili@mail.ru>
2021-10-03 23:20:05 +0300

    Fix 1_weather.py

--- 1_weather.py
+++ 1_weather.py
@@ -2,12 +2,23 @@ import requests
 
 
 url_template = "http://wttr.in/{}"
-settings = {"london":  "en", "svo": "ru", "Череповец": "ru"}
 
-for location in settings.keys():
+settings = {
+    "london":  "en",
+    "svo": "ru",
+    "Череповец": "ru",
+}
+
+for location, language in settings.items():
     url = url_template.format(location)
 
-    payload = {"n": "", "m": "", "T": "", "lang": settings[location]}
+    payload = {
+        "n": "",
+        "m": "",
+        "T": "",
+        "q": "",
+        "lang": language,
+    }
 
     response = requests.get(url, params=payload)
     response.raise_for_status()
